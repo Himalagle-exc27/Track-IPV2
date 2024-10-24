b06842edd5a1dca8c47419ef75dbe3df2ace392a
Emperor Skyhima <slavaseannextgeneration@gmail.com>
2024-10-24 17:00:21 +0800

    Update update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+clear
+figlet -f shadow "Updating..." | lolcat
+echo "[•] Wait while updating" | lolcat
+pkg update && pkg upgrade
+xdg-open "https://www.youtube.com/@Skyhima404-Net"
+pip install lolcat rainbow colorama
+xdg-open "https://wa.me/+6281219197281"
+echo "[✔] The process is ready waiting to open the script" | lolcat
+git clone https://github.com/Himalagle-exc27/Track-IPV2
+cd Track-IPV2
+bash track.sh
